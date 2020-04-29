86e1a0b2d8111ab329788c9c7a976967d9eb40e4
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-04-29 22:58:04 +0430

    Inserting order data with SimpleJdbcInsert wrapper class

--- src/main/java/dev/mohsenkohan/tacocloud/controller/OrderController.java
+++ src/main/java/dev/mohsenkohan/tacocloud/controller/OrderController.java
@@ -1,6 +1,7 @@
 package dev.mohsenkohan.tacocloud.controller;
 
 import dev.mohsenkohan.tacocloud.domain.Order;
+import dev.mohsenkohan.tacocloud.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,27 +9,38 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
+
+    private final OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
 
     @GetMapping("/current")
     public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(@Valid Order order, Errors errors,
+                               SessionStatus sessionStatus) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
-        log.info("Order submitted: " + order);
+        orderRepository.save(order);
+        sessionStatus.setComplete();
+
         return "redirect:/";
     }
 }
---
+++ src/main/java/dev/mohsenkohan/tacocloud/repository/JdbcOrderRepository.java
@@ -0,0 +1,64 @@
+package dev.mohsenkohan.tacocloud.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.mohsenkohan.tacocloud.domain.Order;
+import dev.mohsenkohan.tacocloud.domain.Taco;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class JdbcOrderRepository implements OrderRepository {
+
+    private final SimpleJdbcInsert orderInserter;
+    private final SimpleJdbcInsert orderTacoInserter;
+    private final ObjectMapper objectMapper;
+
+    public JdbcOrderRepository(JdbcTemplate jdbc) {
+        orderInserter = new SimpleJdbcInsert(jdbc)
+                .withTableName("Taco_Order")
+                .usingGeneratedKeyColumns("id");
+
+        orderTacoInserter = new SimpleJdbcInsert(jdbc)
+                .withTableName("Taco_Order_Tacos");
+
+        objectMapper = new ObjectMapper();
+    }
+
+    @Override
+    public Order save(Order order) {
+        order.setPlacedAt(Instant.now());
+
+        long orderId = saveOrderDetails(order);
+        order.setId(orderId);
+
+        for (Taco taco : order.getTacos()) {
+            saveTacoToOrder(taco, orderId);
+        }
+
+        return order;
+    }
+
+    private long saveOrderDetails(Order order) {
+        Map<String, Object> values =
+                objectMapper.convertValue(order, Map.class);
+        values.put("placedAt", order.getPlacedAt());
+
+        long orderId =
+                orderInserter.executeAndReturnKey(values).longValue();
+
+        return orderId;
+    }
+
+    private void saveTacoToOrder(Taco taco, long orderId) {
+        Map<String, Object> values = new HashMap<>();
+        values.put("tacoOrder", orderId);
+        values.put("taco", taco.getId());
+
+        orderTacoInserter.execute(values);
+    }
+}
