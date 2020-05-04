ba93d2f5253f602941b1bb5c277e9eb0ab15e81f
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-05-04 16:56:12 +0430

    Configuring custom user details service with spring security

--- src/main/java/dev/mohsenkohan/tacocloud/configuration/SecurityConfig.java
+++ src/main/java/dev/mohsenkohan/tacocloud/configuration/SecurityConfig.java
@@ -1,29 +1,32 @@
 package dev.mohsenkohan.tacocloud.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import javax.sql.DataSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    DataSource dataSource;
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery(
-                        "SELECT username, password, enabled FROM users WHERE username = ?")
-                .authoritiesByUsernameQuery(
-                        "SELECT username, authority FROM authorities WHERE username = ?")
-                .passwordEncoder(new BCryptPasswordEncoder());
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
     }
 }
