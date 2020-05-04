9a23ca63ffb11e18d3e65b8ac80c57035feab45b
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-05-04 19:57:54 +0430

    Securing requests

--- src/main/java/dev/mohsenkohan/tacocloud/configuration/SecurityConfig.java
+++ src/main/java/dev/mohsenkohan/tacocloud/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package dev.mohsenkohan.tacocloud.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -28,5 +29,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(encoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .access("hasRole('USER')")
+                .antMatchers("/", "/**")
+                .access("permitAll()");
     }
 }
