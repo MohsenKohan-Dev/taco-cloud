b51a9d2d1788c3e7cd9f0b0beabbdf0ff5c801a4
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-05-04 15:31:40 +0430

    Replacing in-memory user store with jdbc user store

--- src/main/java/dev/mohsenkohan/tacocloud/configuration/SecurityConfig.java
+++ src/main/java/dev/mohsenkohan/tacocloud/configuration/SecurityConfig.java
@@ -1,23 +1,29 @@
 package dev.mohsenkohan.tacocloud.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("buzz")
-                .password("{noop}infinity")
-                .authorities("ROLE_USER")
-                .and()
-                .withUser("woody")
-                .password("{noop}bullseye")
-                .authorities("ROLE_USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "SELECT username, password, enabled FROM users WHERE username = ?")
+                .authoritiesByUsernameQuery(
+                        "SELECT username, authority FROM authorities WHERE username = ?")
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 }
