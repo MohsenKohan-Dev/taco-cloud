d27edc8819614b72929fe971c56397b99544ab8c
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-05-04 16:29:35 +0430

    Creating a user details service

--- src/main/java/dev/mohsenkohan/tacocloud/repository/UserRepository.java
+++ src/main/java/dev/mohsenkohan/tacocloud/repository/UserRepository.java
@@ -3,7 +3,9 @@ package dev.mohsenkohan.tacocloud.repository;
 import dev.mohsenkohan.tacocloud.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/dev/mohsenkohan/tacocloud/service/UserRepositoryUserDetailsService.java
@@ -0,0 +1,27 @@
+package dev.mohsenkohan.tacocloud.service;
+
+import dev.mohsenkohan.tacocloud.domain.User;
+import dev.mohsenkohan.tacocloud.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserRepositoryUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserRepositoryUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        return user.orElseThrow(
+                () -> new UsernameNotFoundException("User '" + username + "' not found"));
+    }
+}
